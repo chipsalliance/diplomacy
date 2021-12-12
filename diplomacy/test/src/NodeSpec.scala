70e2a9a21b1f96c006404227ceb5e875598aeab0
JACKLIAO0 <1362427803@qq.com>
2021-12-12 14:34:42 +0000

    add a simple usage of BundleBridgeNexus in test

--- diplomacy/test/src/NodeSpec.scala
+++ diplomacy/test/src/NodeSpec.scala
@@ -1,14 +1,15 @@
 package diplomacy.unittest
 
 import chipsalliance.rocketchip.config.Parameters
-import chisel3._
-import diplomacy.bundlebridge.{BundleBridgeSink, BundleBridgeSource}
+import chisel3.{Data, _}
+import diplomacy.bundlebridge.BundleBridgeNexus.{fillN, orReduction}
+import diplomacy.bundlebridge.{BundleBridgeNexus, BundleBridgeNexusNode, BundleBridgeSink, BundleBridgeSource}
 import diplomacy.lazymodule.{LazyModule, LazyModuleImp}
 import utest._
 
 object NodeSpec extends TestSuite {
   def tests: Tests = Tests {
-    test("raw BundleBridge prototype.") {
+    test("raw BundleBridge Source and Sink prototype.") {
       implicit val p = Parameters.empty
       val genOption = () => UInt(32.W)
 
@@ -37,8 +38,7 @@ object NodeSpec extends TestSuite {
       println(chisel3.stage.ChiselStage.emitSystemVerilog(LazyModule(new TopLazyModule).module))
     }
 
-
-    test("BundleBridge normal usage"){
+    test("BundleBridge Source and Sink normal usage"){
       implicit val p = Parameters.empty
       class BottomLazyModule extends LazyModule {
         val source = BundleBridgeSource(() => UInt(32.W))
@@ -56,5 +56,41 @@ object NodeSpec extends TestSuite {
       }
       println(chisel3.stage.ChiselStage.emitSystemVerilog(LazyModule(new TopLazyModule).module))
     }
+
+    test("BundleBridge Nexus prototype and normal usage") {
+      implicit val p = Parameters.empty
+      class ExampleLazymodule[T<: Data] extends LazyModule {
+
+        val aname: Option[String] = None
+        val registered: Boolean = false
+        val default: Option[() => T] = None
+        val inputRequiresOutput: Boolean = false // when false, connecting a source does not mandate connecting a sink
+        val canshouldBeInlined: Boolean = true
+
+        val broadcast: BundleBridgeNexus[T] = LazyModule(
+          new BundleBridgeNexus[T](
+            inputFn = BundleBridgeNexus.requireOne[T](registered),
+            outputFn = BundleBridgeNexus.fillN[T](registered),
+            default = default,
+            inputRequiresOutput = inputRequiresOutput,
+            shouldBeInlined = canshouldBeInlined
+          )(p)
+        )
+        val broadcastname = aname.foreach(broadcast.suggestName)
+        //def return a node
+        val broadcastnode = broadcast.node
+
+        lazy val module = new LazyModuleImp(this) {}
+
+      }
+      val ExampleLM = LazyModule(new ExampleLazymodule())
+      chisel3.stage.ChiselStage.elaborate(ExampleLM.module)
+      println(ExampleLM.suggestedName)
+      println(ExampleLM.broadcastname)
+      println(ExampleLM.broadcastnode)
+
+
+    }
+
   }
 }
