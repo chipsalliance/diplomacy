109157c78bf52752c24fe7615c1176ca93c1eb41
JACKLIAO0 <1362427803@qq.com>
2021-12-13 12:09:43 +0000

    add a test of BundleBridgeNexus"

--- diplomacy/test/src/NodeSpec.scala
+++ diplomacy/test/src/NodeSpec.scala
@@ -36,6 +36,7 @@ object NodeSpec extends TestSuite {
         lazy val module = new LazyModuleImp(this){}
       }
       println(chisel3.stage.ChiselStage.emitSystemVerilog(LazyModule(new TopLazyModule).module))
+      //chisel3.stage.ChiselStage.emitSystemVerilog(LazyModule(new TopLazyModule).module)
     }
 
     test("BundleBridge Source and Sink normal usage"){
@@ -59,11 +60,31 @@ object NodeSpec extends TestSuite {
 
     test("BundleBridge Nexus prototype and normal usage") {
       implicit val p = Parameters.empty
-      class ExampleLazymodule[T<: Data] extends LazyModule {
+      val genOption = () => UInt(32.W)
 
-        val aname: Option[String] = None
+      class DemoSource(implicit valName: sourcecode.Name) extends BundleBridgeSource[UInt](Some(genOption))
+      class DemoSink(implicit valName: sourcecode.Name) extends BundleBridgeSink[UInt](Some(genOption))
+
+      class SourceLazyModule extends LazyModule {
+        val source = new DemoSource
+        lazy val module = new LazyModuleImp(this){
+          source.bundle := 4.U
+        }
+      }
+
+      class SinkLazyModule extends LazyModule {
+        val sink = new DemoSink
+        lazy val module = new LazyModuleImp(this){
+          printf(p"${sink.bundle}")
+          chisel3.assert(sink.bundle === 4.U)
+        }
+      }
+
+      class NexusLazymodule[T <: Data](genOpt: Option[() => T] = None) extends LazyModule {
+
+        val aname: Option[String] = Some("X")
         val registered: Boolean = false
-        val default: Option[() => T] = None
+        val default: Option[() => T] = genOpt
         val inputRequiresOutput: Boolean = false // when false, connecting a source does not mandate connecting a sink
         val canshouldBeInlined: Boolean = true
 
@@ -80,16 +101,35 @@ object NodeSpec extends TestSuite {
         //def return a node
         val broadcastnode = broadcast.node
 
-        lazy val module = new LazyModuleImp(this) {}
-
+        lazy val module = new LazyModuleImp(this) {
+          printf(p"${broadcast.node.out}")
+          printf(p"${broadcast.node.bundleIn}")
+          printf(p"${broadcast.node.bundleOut}")
+          //chisel3.assert(sink.bundle === 4.U)
+        }
       }
-      val ExampleLM = LazyModule(new ExampleLazymodule())
-      chisel3.stage.ChiselStage.elaborate(ExampleLM.module)
-      println(ExampleLM.suggestedName)
-      println(ExampleLM.broadcastname)
-      println(ExampleLM.broadcastnode)
 
+      class TopLazyModule extends LazyModule {
+        val sourceModule = LazyModule(new SourceLazyModule)
+        val sinkModule = LazyModule(new SinkLazyModule)
+        //val OthersinkModule = LazyModule(new SinkLazyModule)
+        val NexusLM =LazyModule(new NexusLazymodule[UInt](Some(genOption)))
+        NexusLM.broadcastnode :*= sourceModule.source
+        sinkModule.sink :*= sourceModule.source
+        //OthersinkModule.sink :=* NexusLM.broadcastnode
+        lazy val module = new LazyModuleImp(this){
+        }
+      }
 
+      val TopLM =  LazyModule(new TopLazyModule())
+      val NexusLM = LazyModule(new NexusLazymodule[UInt](Some(genOption)))
+      //chisel3.stage.ChiselStage.elaborate(ExampleLM.broadcast.module)
+      chisel3.stage.ChiselStage.elaborate(TopLM.module)
+      println(chisel3.stage.ChiselStage.emitSystemVerilog(LazyModule(new TopLazyModule).module))
+      println(TopLM.NexusLM.broadcastnode)
+      println(TopLM.NexusLM.broadcastname)
+      println(NexusLM.broadcastnode)
+      println(NexusLM.broadcastname)
     }
 
   }
