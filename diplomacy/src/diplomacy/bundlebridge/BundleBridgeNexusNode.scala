fd90eb0784cc2e1e9857d3208380cf6856ce2ef1
Alex Swehla <alex@zephyrcomputing.space>
2024-02-07 22:17:45 -0800

    Pull changes from rocket-chip/dev into Diplomacy
    This branch incorporates changes from mainline rocket-chip,
    sequencer/diplomacy, and chipsalliance/diplomacy. The goal
    is to finish the effort to break diplomacy out into a standalone
    library.
    
    As part of this effort we have split a fresh set of history from the
    current dev branch on rocket-chip (https://github.com/chipsalliance/rocket-chip/commit/baa9f4523).
    The previous effort has been moved to the master-archive branch for
    preservation, and this branch will become main going forward.
    From this updated history we have done our best to roll all the changes
    in the chipsalliance and sequencer diplomacy repos forward.
    This creates a composite of the current known functional diplomacy code and any changes
    and refactors that were previously applied.

---
+++ diplomacy/src/diplomacy/bundlebridge/BundleBridgeNexusNode.scala
@@ -0,0 +1,17 @@
+package org.chipsalliance.diplomacy.bundlebridge
+
+import chisel3.Data
+import org.chipsalliance.diplomacy.nodes.NexusNode
+
+case class BundleBridgeNexusNode[T <: Data](
+  default:             Option[() => T] = None,
+  inputRequiresOutput: Boolean = false
+) // when false, connecting a source does not mandate connecting a sink
+(
+  implicit valName:    sourcecode.Name)
+    extends NexusNode(new BundleBridgeImp[T])(
+      dFn = seq => seq.headOption.getOrElse(BundleBridgeParams(default)),
+      uFn = seq => seq.headOption.getOrElse(BundleBridgeParams(None)),
+      inputRequiresOutput = inputRequiresOutput,
+      outputRequiresInput = !default.isDefined
+    )
